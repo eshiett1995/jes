c7644a4cb84dbe01d656396ecbf2e5ef1306ad23
eshiett1995@gmail.com <Iam2fresh>
2021-04-14 15:10:33 +0100

    work in progress

--- src/main/kotlin/JES.kt
+++ src/main/kotlin/JES.kt
@@ -1,29 +1,35 @@
-class JES(data : String, key : String) {
+import kotlin.properties.Delegates
+
+class JES(var data : String, var key : String) {
 
     fun encrypt() : String{
+       var subbedHexArray = mutableListOf<String>()
+       for (char in data){
+           val charArray = Integer.toHexString(char.toInt()).toCharArray()
+           val newHex = hexSub(charArray[0].toString(), charArray[1].toString())
+           subbedHexArray.add(newHex)
+           println("${Integer.toHexString(char.toInt())} - $newHex")
+
+       }
        return ""
     }
 
-    fun hexSub() : String{
+    fun decrypt() : String{
         return ""
     }
 
-    var hexMatrix = arrayOf(
-        arrayOf("63","7c","77","7b","f2","6b","6f","c5","30","01","67","2b","fe","d7","ab","76"),
-        arrayOf("ca","82","c9","7d","fa","59","47","f0","ad","d4","a2","af","9c","a4","72","c0"),
-        arrayOf("b7","fd","93","26","36","3f","f7","cc","34","a5","e5","f1","71","d8","31","15"),
-        arrayOf("04","c7","23","c3","18","96","05","9a","07","12","80","e2","eb","27","b2","75"),
-        arrayOf("09","83","2c","1a","1b","6e","5a","a0","52","3b","d6","b3","29","e3","2f","84"),
-        arrayOf("53","d1","00","ed","20","fc","b1","5b","6a","cb","be","39","4a","4c","58","cf"),
-        arrayOf("d0","ef","aa","fb","43","4d","33","85","45","f9","02","7f","50","3c","9f","a8"),
-        arrayOf("51","a3","40","8f","92","9d","38","f5","bc","b6","da","21","10","ff","f3","d2"),
-        arrayOf("cd","0c","13","ec","5f","97","44","17","c4","a7","7e","3d","64","5d","19","73"),
-        arrayOf("60","81","4f","dc","22","2a","90","88","46","ee","b8","14","de","5e","0b","db"),
-        arrayOf("e0","32","3a","0a","49","06","24","5c","c2","d3","ac","62","91","95","e4","79"),
-        arrayOf("e7","c8","37","6d","8d","d5","4e","a9","6c","56","f4","ea","65","7a","ae","08"),
-        arrayOf("ba","78","25","2e","1c","a6","b4","c6","e8","dd","74","1f","4b","bd","8b","8a"),
-        arrayOf("70","3e","b5","66","48","03","f6","0e","61","35","57","b9","86","c1","1d","9e"),
-        arrayOf("e1","f8","98","11","69","d9","8e","94","9b","1e","87","e9","ce","55","28","df"),
-        arrayOf("8c","a1","89","0d","bf","e6","42","68","41","99","2d","0f","b0","54","bb","16"),
-    )
+    fun hexSub(row : String, column : String) : String{
+        var c = data;
+        val indexOfColumn = Constants.hexMatrix[0].indexOf(column)
+        var indexOfRow by Delegates.notNull<Int>()
+        for ((index,array) in Constants.hexMatrix.withIndex()){
+             if(array[0] == row){
+                 indexOfRow = index
+                 break
+             }else{
+                 continue
+             }
+        }
+        return Constants.hexMatrix[indexOfRow][indexOfColumn]
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,4 @@
 fun main(args: Array<String>) {
-    JES("this is where it all starts", "charles")
+    JES("this is where it all starts", "charles").encrypt()
 }
 
