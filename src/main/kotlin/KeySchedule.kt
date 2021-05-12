8ae828c8cb5b5e636c8403aef5da0be84a04549e
eshiett1995@gmail.com <Iam2fresh>
2021-05-12 15:17:09 +0100

    Update KeySchedule.kt

--- src/main/kotlin/KeySchedule.kt
+++ src/main/kotlin/KeySchedule.kt
@@ -1,12 +1,19 @@
 import kotlin.properties.Delegates
 
 class KeySchedule<T> (var key : String){
-    var scheduler : HashMap<String, ArrayList<T>> = HashMap()
+    private var scheduler : HashMap<Int, ArrayList<ArrayList<String>>> = HashMap()
 
     fun convert(){
-        var hexList = swapForHexValue()
-        arrangeAndPushToScheduler(hexList)
+        val hexList = swapForHexValue()
+        val firstSchedule = arrangeToColumnAndPushToScheduler(hexList)
+        scheduler[1] = firstSchedule
+        makeNextSchedule()
     }
+
+    private fun makeNextSchedule(){
+        //scheduler.keys.
+    }
+
     private fun swapForHexValue() : MutableList<String>{
         val subbedHexArray = mutableListOf<String>()
         for (char in key){
@@ -31,11 +38,32 @@ class KeySchedule<T> (var key : String){
         return Constants.hexMatrix[indexOfRow][indexOfColumn]
     }
 
-    private fun arrangeAndPushToScheduler(hexList: List<String>){
-        var tempList = mutableListOf<String>()
+    private fun arrangeToColumnAndPushToScheduler(hexList: List<String>) : ArrayList<ArrayList<String>>{
+        val finalList = ArrayList<ArrayList<String>>()
+        val rowOneList = ArrayList<String>()
+        val rowTwoList = ArrayList<String>()
+        val rowThreeList = ArrayList<String>()
+        val rowFourList = ArrayList<String>()
         for((index,hex) in hexList.withIndex()){
-            tempList.add(hex)
-            if(index ==)
+
+            if(index == 0 || index == 4 || index == 8 || index == 12){
+                rowOneList.add(hex)
+            }
+            if(index == 1 || index == 5 || index == 9 || index == 13){
+                rowTwoList.add(hex)
+            }
+            if(index == 2 || index == 6 || index == 10 || index == 14){
+                rowThreeList.add(hex)
+            }
+            if(index == 3 || index == 7 || index == 11 || index == 15){
+                rowFourList.add(hex)
+            }
         }
+
+        finalList.add(rowOneList)
+        finalList.add(rowTwoList)
+        finalList.add(rowThreeList)
+        finalList.add(rowFourList)
+        return finalList
     }
 }
