63445d758c3d28db83124d71e9c137c9e87f137b
eshiett1995@gmail.com <Iam2fresh>
2021-06-08 00:47:44 +0100

    work in progress

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val result = JES("hello", "thisissparta1234").encrypt()
+    val result = JES("hello bassey", "thisissparta1234").encrypt()
     println(result)
 
     val decryptedResult = JES(result, "thisissparta1234").decrypt()
